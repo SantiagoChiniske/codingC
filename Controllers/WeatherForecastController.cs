13a707f0e131df6b8d4f05fc8bc5ab1477610b41
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2022-09-17 11:52:17 -0300

    penultimo

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,23 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-
+using static  System.IO.File;
 namespace codingC.Controllers;
 
 [ApiController]
-[Route("test")]
+
 public class WeatherForecastController : ControllerBase
 {
-    [HttpGet("Oi")]
-   public ActionResult outrotest(){
-       return Content(@"
-           <marquee>
-                <h1> 
-                Hello World Estamos dominando tudo rapaz
-                </h1>
-            </marquee>", "text/html") ;
-   }
     [HttpGet]
-    public string Test(){
-        return "Meu backend está rodando";
+   public ActionResult index(){
+       
 
-    }
+       return Content(ReadAllText("view/index.html"), "text/html") ;
+   }
+   [HttpGet()]
+   public ActionResult Login(){
+       
+
+       return Content(ReadAllText("view/index.html"), "text/html") ;
+   }
+    
 }
