d9d083220ee3405d67769eb526cc84e35f161be8
Santiago Chiniske Pereira <santiagochiniske10@gmail.com>
2022-09-17 11:34:41 -0300

    texto

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -6,5 +6,18 @@ namespace codingC.Controllers;
 [Route("test")]
 public class WeatherForecastController : ControllerBase
 {
+    [HttpGet("Oi")]
+   public ActionResult outrotest(){
+       return Content(@"
+           <marquee>
+                <h1> 
+                Hello World Estamos dominando tudo rapaz
+                </h1>
+            </marquee>", "text/html") ;
+   }
+    [HttpGet]
+    public string Test(){
+        return "Meu backend está rodando";
 
+    }
 }
